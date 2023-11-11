d5873682046d6cd1cba947648852284654d8833a
Praveen-D-0 <jopraveen06@gmail.com>
2023-11-11 11:51:27 +0530

    adding multiple components

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,20 @@
-function App() {
+const Hello = (props) => {
+  console.log(props);
+  return (
+    <p>Hello {props.name} , you are {props.age}</p>
+  )
+}
+
+
+function App() { 
+  const age = 25;
 
   return (
   <>
-   <h1>App component</h1>
-   <p>Hello world</p>
+   <h1>Greetings</h1>
+   <p>Hello Guest</p>
+   <Hello name ='sathish' age={10+20} />
+   <Hello name='Krish' age={age} />
   </>)
 }
 
